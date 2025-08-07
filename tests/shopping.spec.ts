f69a1cdcd4e80614dec9ac6a21529a8609f2d131
vbludov <ext-viktor.bludov@flowbird.group>
2025-08-07 18:05:29 +0300

    Moved product buying steps to separate function

--- tests/shopping.spec.ts
+++ tests/shopping.spec.ts
@@ -1,23 +1,24 @@
 import { expect, test } from '../fixtures/main'
 import fs from 'fs'
+import { SearchResultsPage } from '../pages/shopping/searchResultsPage'
+import { ProductPage } from '../pages/shopping/productPage'
+import { CheckoutPage } from '../pages/shopping/checkoutPage'
 
 const userFile = '.auth/user/shopping/customer.json'
 const productsFile = '.db/products.json'
 const registeredCustomer = JSON.parse(fs.readFileSync(userFile, 'utf-8'))
 const availableProducts = JSON.parse(fs.readFileSync(productsFile, 'utf-8'))
 
-test('pick product from catalog and proceed to payment @regression', async ({ homePage, searchResultsPage, productPage, checkoutPage }) => {
-    const parentCategory = 'Components'
-    const category = 'Monitors'
+/**
+ * Executes product buying steps, common for different tests
+ * @param searchResultsPage 
+ * @param productPage 
+ * @param checkoutPage 
+ */
+async function buyProduct(searchResultsPage: SearchResultsPage, productPage: ProductPage, checkoutPage: CheckoutPage) {
     const purchaseQty = 2
 
-    await homePage.expandCategoryTypeDropdown(parentCategory)
-    await homePage.clickCategory(category)
-    await expect(searchResultsPage.parentCategoryBreadcrumb, 'Chosen category type should be displayed in breadcrumbs').toHaveText(parentCategory)
-    await expect(searchResultsPage.categoryBreadcrumb, 'Chosen category should be displayed in breadcrumbs').toHaveText(category)
     const productName = await searchResultsPage.productTitle.innerText()
-    
-    // TODO: move following section (to fixture) so that it can be reused in other tests
     await searchResultsPage.openProductPage()
     expect(productName, 'Product name should match name in search results').toEqual(await productPage.productTitle.innerText())
 
@@ -33,7 +34,18 @@ test('pick product from catalog and proceed to payment @regression', async ({ ho
     await productPage.proceedToCheckout()
     await expect(checkoutPage.pageTitle).toHaveText('Checkout')
     // payment steps not implemented due to broken opencart's endpoints
-    // end of TODO
+}
+
+test('pick product from catalog and proceed to payment @regression', async ({ homePage, searchResultsPage, productPage, checkoutPage }) => {
+    const parentCategory = 'Components'
+    const category = 'Monitors'
+
+    await homePage.expandCategoryTypeDropdown(parentCategory)
+    await homePage.clickCategory(category)
+    await expect(searchResultsPage.parentCategoryBreadcrumb, 'Chosen category type should be displayed in breadcrumbs').toHaveText(parentCategory)
+    await expect(searchResultsPage.categoryBreadcrumb, 'Chosen category should be displayed in breadcrumbs').toHaveText(category)
+    
+    await buyProduct(searchResultsPage, productPage, checkoutPage)
 })
 
 test('search for product, pick from search results and proceed to payment', async ({ homePage }) => {
